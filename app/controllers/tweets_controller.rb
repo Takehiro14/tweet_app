8fb332232f06818162bca3ef441bbae0797407d1
takehiro-tsukagoshi <takehiro.plusultra@gmail.com>
2020-04-17 18:27:53 +0900

    詳細ページの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
@@ -14,7 +15,6 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
@@ -29,10 +29,17 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
 end
