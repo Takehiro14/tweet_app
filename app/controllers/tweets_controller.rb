e8fd611d4991656d9ff2b08eefdb0e0af6d3fd41
takehiro-tsukagoshi <takehiro.plusultra@gmail.com>
2020-04-17 18:51:21 +0900

    create users_controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
@@ -40,6 +41,10 @@ class TweetsController < ApplicationController
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
