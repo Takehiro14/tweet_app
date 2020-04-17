6c31d0ad7befdf92ad12d92ce03b654a124e5cc0
takehiro-tsukagoshi <takehiro.plusultra@gmail.com>
2020-04-17 17:46:35 +0900

    add new.html.haml

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,20 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
 end
